b10e7851d0fb4d5d180e1907646d91c8d4e5a961
Cojay77 <corentin.jayer@gmail.com>
2025-04-17 16:06:10 +0200

    🧹 Cleanup gh-pages

--- lib/utils/platform_utils_web.dart
+++
@@ -1,7 +0,0 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
-
-bool isIOSBrowser() {
-  final userAgent = html.window.navigator.userAgent.toLowerCase();
-  return userAgent.contains('iphone') || userAgent.contains('ipad');
-}
